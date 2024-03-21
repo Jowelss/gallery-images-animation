cba0c261e2ae1dedb2d22942b714ba80636e561c
Jowelss <devejowels.gitl.loper@gmail.com>
2024-03-21 19:53:05 -0400

    added events and  animations

--- js/app.js
+++ js/app.js
@@ -1,9 +1,25 @@
-const images = document.querySelectorAll('.image-content');
-const title = document.querySelector('.title');
+const images = document.querySelectorAll('.image');
+const titleBestia = document.querySelector('.title-bestia');
+const titleBestiaInfo = document.querySelector('.titan-bestia__info');
+const titleAcorazado = document.querySelector('.title-acorazado');
 
 images.forEach((image) => {
-  image.addEventListener('click', () => {
-    image.style.filter = ' brightness(10%)';
-    title.style.top = '0'; // Prueba para hacer que titulo se vaya para arriba, falta transicion y aplicarla a todos los titles
+  image.addEventListener('click', (imageEvent) => {
+    imageEvent.target.style.filter = 'brightness(20%)';
+    imageEvent.target.style.transition = '1s';
+
+    if (imageEvent.target.classList[1] === 'titan-bestia') {
+      titleBestia.style.bottom = '360px';
+      titleBestia.style.fontSize = '1.3rem';
+      titleBestiaInfo.style.transition = '1s';
+      titleBestiaInfo.style.transitionDelay = '0.6s';
+      titleBestiaInfo.style.zIndex = '1';
+      titleBestiaInfo.style.opacity = '1';
+    }
+
+    if (imageEvent.target.classList[1] === 'titan-acorazado') {
+      titleAcorazado.style.bottom = '300px';
+      titleAcorazado.style.fontSize = '1.3rem';
+    }
   });
 });
