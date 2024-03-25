a8ffa258954e2b614841a04ba8be794bd1edce16
Jowelss <devejowels.gitl.loper@gmail.com>
2024-03-25 19:54:51 -0400

    new ifelse for titanAcorazado

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
-const images = document.querySelectorAll('.image');
-const imageContent = document.querySelectorAll('.image-content');
+const images = document.querySelectorAll('.image-content');
 const titleBestia = document.querySelector('.title-bestia');
 const titanBestiaInfo = document.querySelector('.titan-bestia__info');
 const titleAcorazado = document.querySelector('.title-acorazado');
@@ -10,11 +9,11 @@ let activatorAcorazado = true;
 
 images.forEach((image) => {
   image.addEventListener('click', (imageEvent) => {
-    imageEvent.target.style.filter = 'brightness(20%)';
-    imageEvent.target.style.transition = '1s';
-
     if (imageEvent.target.classList[1] === 'titan-bestia' && activatorBestia) {
-      titleBestia.style.bottom = '340px';
+      console.log(imageEvent.target);
+      imageEvent.target.style.filter = 'brightness(20%)';
+      imageEvent.target.style.transition = '1s';
+      titleBestia.style.bottom = '330px';
       titleBestia.style.fontSize = '1.3rem';
       titanBestiaInfo.style.transition = '1s';
       titanBestiaInfo.style.transitionDelay = '0.7s';
@@ -27,16 +26,36 @@ images.forEach((image) => {
       titleBestia.style.bottom = '0px';
       titleBestia.style.fontSize = '2rem';
       titanBestiaInfo.style.zIndex = '-1';
-      titanBestiaInfo.style.transition = '0.2s';
+      titanBestiaInfo.style.transition = '0.3s';
       titanBestiaInfo.style.opacity = '0';
 
       activatorBestia = true;
     }
 
-    image.addEventListener('mouseover', () => {
-      titanBestiaInfo.style.transition = '0.2s';
-      titanBestiaInfo.style.zIndex = '-1';
-      titanBestiaInfo.style.opacity = '0';
-    });
+    if (
+      imageEvent.target.classList[1] === 'titan-acorazado' &&
+      activatorAcorazado
+    ) {
+      console.log(imageEvent.target);
+      imageEvent.target.style.filter = 'brightness(20%)';
+      imageEvent.target.style.transition = '1s';
+      titleAcorazado.style.bottom = '370px';
+      titleAcorazado.style.fontSize = '1.3rem';
+      titanAcorazadoInfo.style.transition = '1s';
+      titanAcorazadoInfo.style.transitionDelay = '0.7s';
+      titanAcorazadoInfo.style.zIndex = '1';
+      titanAcorazadoInfo.style.opacity = '1';
+
+      activatorAcorazado = false;
+    } else {
+      imageEvent.target.style.filter = 'brightness(100%)';
+      titleAcorazado.style.bottom = '0px';
+      titleAcorazado.style.fontSize = '2rem';
+      titanAcorazadoInfo.style.zIndex = '-1';
+      titanAcorazadoInfo.style.transition = '0.3s';
+      titanAcorazadoInfo.style.opacity = '0';
+
+      activatorAcorazado = true;
+    }
   });
 });
