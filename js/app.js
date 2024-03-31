b003d199bf4fc4dde9816c1a0a62cf72798df81b
Jowelss <devejowels.gitl.loper@gmail.com>
2024-03-30 21:57:33 -0400

    reduce code, more legible

--- js/app.js
+++ js/app.js
@@ -1,11 +1,23 @@
 const template = document.getElementById('template');
 const titanContent = document.querySelectorAll('.titan-content');
-
-const store = [];
-
-const getData = () => {
+const fragment = document.createDocumentFragment();
+let activador = true;
+const getData = (labels) => {
   const copyTemplate = document.importNode(template.content, true);
-  for (const elemento of copyTemplate.children) {
-    store.push({ id: elemento.id });
+  const element = copyTemplate.querySelectorAll('.info');
+  for (let i = 0; i < element.length; i++) {
+    if (labels.id === element[i].id && activador) {
+      const coso = element[i];
+      coso.style.transition = '2s';
+      labels.appendChild(coso);
+    }
   }
 };
+
+for (const label of titanContent) {
+  label.addEventListener('click', (e) => {
+    const child = e.currentTarget.lastElementChild;
+    child.style.bottom = '360px';
+    return getData(e.currentTarget);
+  });
+}
