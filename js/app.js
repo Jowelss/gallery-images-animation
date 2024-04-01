69201f6c872f221298207d8e4418375d9d3cc2a7
Jowelss <devejowels.gitl.loper@gmail.com>
2024-04-01 00:16:35 -0400

    continue tomorrow

--- js/app.js
+++ js/app.js
@@ -1,15 +1,17 @@
 const template = document.getElementById('template');
 const titanContent = document.querySelectorAll('.titan-content');
 const fragment = document.createDocumentFragment();
-let activador = true;
+
 const getData = (labels) => {
   const copyTemplate = document.importNode(template.content, true);
   const element = copyTemplate.querySelectorAll('.info');
   for (let i = 0; i < element.length; i++) {
-    if (labels.id === element[i].id && activador) {
+    if (labels.id === element[i].id) {
       const coso = element[i];
-      coso.style.transition = '2s';
       labels.appendChild(coso);
+      if (coso > 2) {
+        coso.remove('id');
+      }
     }
   }
 };
